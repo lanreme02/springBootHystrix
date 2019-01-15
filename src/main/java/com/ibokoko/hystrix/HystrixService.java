18304ba5f03f78074893102cffa1dac84ae0c1c2
olanrewaju Elisha <mesg4lanre@hotmail.com>
2019-01-14 22:41:22 -0500

    Hytrix client

---
+++ src/main/java/com/ibokoko/hystrix/HystrixService.java
@@ -0,0 +1,31 @@
+package com.ibokoko.hystrix;
+
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+
+@Service
+public class HystrixService {
+
+    private final RestTemplate restTemplate;
+
+    public HystrixService(RestTemplate rest) {
+        this.restTemplate = rest;
+    }
+
+    @HystrixCommand(fallbackMethod = "reliable")
+    public String readingList() {
+        URI uri = URI.create("http://localhost:8080/name");
+
+        return this.restTemplate.getForObject(uri, String.class);
+    }
+
+    public String reliable() {
+        return "Cloud Native Java (O'Reilly)";
+    }
+
+}
